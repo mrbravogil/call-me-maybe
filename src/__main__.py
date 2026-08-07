3bce0a529ab838aaabe5093a1ff71f005e7a1f50
mrbravogil <mrbravogil@gmail.com>
2026-08-07 11:15:25 +0200

    07.08.26

--- src/__main__.py
+++ src/__main__.py
@@ -12,6 +12,16 @@ def parse_args() -> argparse.Namespace:
         '--functions_definition',
         default='data/input/functions_definition.json'
     )
+    parser.add_argument(
+        '--input',
+        default='data/input/function_calling_tests.json'
+    )
+    parser.add_argument(
+        '--output',
+        default='data/output/function_calling_results.json'
+    )
+    return parser.parse_args()
+
 
 def main() -> None:
     print("hola")
