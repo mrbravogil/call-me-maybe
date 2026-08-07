72c043308435418c49cb0fcbad72b8169abb9eb2
mrbravogil <mrbravogil@gmail.com>
2026-08-07 10:56:55 +0200

    07.08.26

---
+++ README.md
@@ -0,0 +1,10 @@
+Call me maybe
+
+
+Dependencias
+uv add numpy pydantic torch transformers
+or
+uv sync
+
+uv add --dev flake8 mypy
+
---
+++ src/main.py
@@ -0,0 +1,14 @@
+import argparse
+import json
+import sys
+from pydantic import BaseModel, ValidationError
+import pathlib
+from llm_sdk import Small_LLM_Model  # type: ignore
+
+
+def main() -> None:
+    print("hola")
+
+
+if __name__ == "__main__":
+    main()
