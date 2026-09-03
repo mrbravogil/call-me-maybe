2fcb3c0783e4d2f100188af77bd549f7074b5315
mrbravogil <mrbravogil@gmail.com>
2026-09-03 13:03:58 +0200

    03.09.26
    fix llms args bug 6

--- src/call_me_maybe.py
+++ src/call_me_maybe.py
@@ -113,10 +113,6 @@ class CallMeMaybe(BaseModel):
                 f'The allowed output keys are exactly: {allowed_keys}.\n'
             )
 
-        disallow_name_line = ''
-        if 'name' not in func.required_params:
-            disallow_name_line = 'The key "name" is not allowed for this function.\n'
-
         instructions: str = (
             '<|im_start|>system\n'
             'Extract arguments for exactly one function from '
@@ -125,8 +121,7 @@ class CallMeMaybe(BaseModel):
             'Do not include the function name.\n'
             'Do not wrap the result inside "arguments".\n'
             'Use exactly the parameter names defined in the schema.\n'
-            + allowed_keys_line +
-            disallow_name_line +
+            + allowed_keys_line
             'Do not add extra keys.\n'
             'Do not rename keys.\n'
             'Do not invent missing values.\n'
